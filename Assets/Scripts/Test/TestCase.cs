7a2e4f70cdb147692faa237cb94bdcded8282084
ximengcs <ximengcs@163.com>
2023-04-26 19:13:44 +0800

    测试代码

--- Assets/Scripts/Test/TestCase.cs
+++ Assets/Scripts/Test/TestCase.cs
@@ -24,16 +24,34 @@ namespace Game.Test
         {
             DebugGUI.Label("Test UI");
             m_Time = DebugGUI.FloatField(m_Time);
-            if (DebugGUI.Button("Init Group"))
+            if (DebugGUI.Button("Init Group1"))
             {
-                UIModule.Inst.MainGroup.AddHelper<MultiUIGroupHelper>((helper) =>
+                UIModule.Inst.MainGroup.AddHelper<OnlyOneUIGroupHelper>((helper) =>
                 {
-                    //helper.SetEffect(new FadeEffect(1, 0.5f), new MoveEffect(MoveEffect.Direct.FromLeft, false, true));
+                    helper.SetEffect(new FadeEffect(1, 0.5f), new MoveEffect(MoveEffect.Direct.FromLeft, false, true));
+                });
+            }
+            if (DebugGUI.Button("Init Group2"))
+            {
+                UIModule.Inst.MainGroup.AddHelper<OnlyOneUIGroupHelper>((helper) =>
+                {
                     helper.SetEffect(
                         new MoveEffect(MoveEffect.Direct.Rand, true, false, m_Time),
                         new MoveEffect(MoveEffect.Direct.Rand, false, true, m_Time));
-                    //helper.SetEffect(new ScaleEffect(Vector2.one), new ScaleEffect(Vector2.one, Vector2.zero));
-                    //helper.SetEffect(new FadeEffect(1, 0.5f), new FadeEffect(1, 0, 0.5f));
+                });
+            }
+            if (DebugGUI.Button("Init Group3"))
+            {
+                UIModule.Inst.MainGroup.AddHelper<OnlyOneUIGroupHelper>((helper) =>
+                {
+                    helper.SetEffect(new ScaleEffect(Vector2.one), new ScaleEffect(Vector2.one, Vector2.zero));
+                });
+            }
+            if (DebugGUI.Button("Init Group4"))
+            {
+                UIModule.Inst.MainGroup.AddHelper<OnlyOneUIGroupHelper>((helper) =>
+                {
+                    helper.SetEffect(new FadeEffect(1, 0.5f), new FadeEffect(1, 0, 0.5f));
                 });
             }
             if (DebugGUI.Button("Open Dialog 1"))
@@ -44,11 +62,6 @@ namespace Game.Test
                 }, true, 1);
                 AudioModule.Inst.PlayAsync("a1.wav");
             }
-            if (DebugGUI.Button("Close Dialog 1"))
-            {
-                UIModule.Inst.Close<DialogUI>(1);
-                AudioModule.Inst.PlayAsync("a2.wav");
-            }
             if (DebugGUI.Button("Open Dialog 2"))
             {
                 UIModule.Inst.Open<DialogUI>((ui) =>
@@ -56,6 +69,11 @@ namespace Game.Test
                     ui.SetData(new Color(0, 0.2f, 0, 1));
                 }, true, 2);
                 AudioModule.Inst.PlayAsync("a1.wav");
+            }
+            if (DebugGUI.Button("Close Dialog 1"))
+            {
+                UIModule.Inst.Close<DialogUI>(1);
+                AudioModule.Inst.PlayAsync("a2.wav");
             }
             if (DebugGUI.Button("Close Dialog 2"))
             {
