2a87f59f34c7e32504c65d49551942b88ea9d893
ximengcs <ximengcs@163.com>
2023-08-18 12:08:34 +0800

    up

--- Assets/TestInput.cs
+++ Assets/TestInput.cs
@@ -9,22 +9,42 @@ public class TestInput : MonoBehaviour
 {
     public RectTransform Target;
     public PlayerInput Map;
+    private InputAction w;
+    private InputAction s;
+    private InputAction a;
+    private InputAction d;
 
     void Start()
     {
         InputAction action = Map.currentActionMap.FindAction("LeftBtn");
         InputAction space = Map.currentActionMap.FindAction("Space");
-        InputAction w = Map.currentActionMap.FindAction("W");
-        InputAction s = Map.currentActionMap.FindAction("S");
-        InputAction a = Map.currentActionMap.FindAction("A");
-        InputAction d = Map.currentActionMap.FindAction("D");
-        w.performed += (context) => Target.anchoredPosition += new Vector2(0f, 5);
-        s.performed += (context) => Target.anchoredPosition += new Vector2(0f, -1 * 5);
-        a.performed += (context) => Target.anchoredPosition += new Vector2(-5, 0);
-        d.performed += (context) => Target.anchoredPosition += new Vector2(5, 0);
+        w = Map.currentActionMap.FindAction("W");
+        s = Map.currentActionMap.FindAction("S");
+        a = Map.currentActionMap.FindAction("A");
+        d = Map.currentActionMap.FindAction("D");
 
         action.performed += InnerBtnLeft;
         space.performed += InnerSpace;
+    }
+
+    private void Update()
+    {
+        if (w.ReadValue<float>() > 0)
+        {
+            Target.anchoredPosition += new Vector2(0f, 5);
+        }
+        if (s.ReadValue<float>() > 0)
+        {
+            Target.anchoredPosition += new Vector2(0f, -1 * 5);
+        }
+        if (a.ReadValue<float>() > 0)
+        {
+            Target.anchoredPosition += new Vector2(-5, 0);
+        }
+        if (d.ReadValue<float>() > 0)
+        {
+            Target.anchoredPosition += new Vector2(5, 0);
+        }
     }
 
     private void InnerBtnLeft(CallbackContext context)
