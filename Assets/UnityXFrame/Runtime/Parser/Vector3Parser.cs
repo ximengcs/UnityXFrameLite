cd52a16fe7e1a92086eb27e93d455b806cb888da
ximengcs <ximengcs@163.com>
2023-08-18 15:17:56 +0800

    add parser

---
+++ Assets/UnityXFrame/Runtime/Parser/Vector3Parser.cs
@@ -0,0 +1,125 @@
+﻿
+using UnityEngine;
+using XFrame.Core;
+using XFrame.Modules.Diagnotics;
+using XFrame.Modules.Pools;
+
+namespace UnityXFrame.Core.Parser
+{
+    public class Vector3Parser : IParser<Vector3>
+    {
+        private Vector3 m_Value;
+        public Vector3 Value => m_Value;
+        public LogLevel LogLv { get; set; }
+
+        object IParser.Value => m_Value;
+
+        int IPoolObject.PoolKey => default;
+
+        public Vector3 Parse(string pattern)
+        {
+            if (string.IsNullOrEmpty(pattern) || !TryParse(pattern, out m_Value))
+            {
+                m_Value = default;
+                Log.Print(LogLv, "XFrame", $"FloatParser parse failure. {pattern}");
+            }
+
+            return m_Value;
+        }
+
+        public static bool TryParse(string pattern, out Vector3 value)
+        {
+            string[] valueStr = pattern.TrimStart('(').TrimEnd(')').Split(',');
+            if (valueStr.Length != 3)
+            {
+                value = default;
+                return false;
+            }
+            else
+            {
+                if (FloatParser.TryParse(valueStr[0], out value.x) &&
+                    FloatParser.TryParse(valueStr[1], out value.y) &&
+                    FloatParser.TryParse(valueStr[2], out value.z))
+                {
+                    return true;
+                }
+                else
+                {
+                    value = default;
+                    return false;
+                }
+            }
+        }
+
+        object IParser.Parse(string pattern)
+        {
+            return Parse(pattern);
+        }
+
+        public override string ToString()
+        {
+            return m_Value.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            IParser parser = obj as IParser;
+            if (parser != null)
+            {
+                return m_Value.Equals(parser.Value);
+            }
+            else
+            {
+                return m_Value.Equals(obj);
+            }
+        }
+
+        void IPoolObject.OnCreate()
+        {
+
+        }
+
+        void IPoolObject.OnRequest()
+        {
+            m_Value = default;
+            LogLv = LogLevel.Warning;
+        }
+
+        void IPoolObject.OnRelease()
+        {
+
+        }
+
+        void IPoolObject.OnDelete()
+        {
+
+        }
+
+        public static bool operator ==(Vector3Parser src, object tar)
+        {
+            return src.Equals(tar);
+        }
+
+        public static bool operator !=(Vector3Parser src, object tar)
+        {
+            return !src.Equals(tar);
+        }
+
+        public static implicit operator Vector3(Vector3Parser parser)
+        {
+            return parser.m_Value;
+        }
+
+        public static implicit operator Vector3Parser(Vector3 value)
+        {
+            Vector3Parser parser = new Vector3Parser();
+            parser.m_Value = value;
+            return parser;
+        }
+    }
+}
